653b0d4003e1deeeca3eb1b336af627f03e7e3b6
Ruchir18 <70945312+Ruchir18@users.noreply.github.com>
2021-01-19 14:29:20 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -105,6 +105,7 @@ class Game{
                     for(var i=0;i<fruitGroup.length;i++)  {
 
                         if(fruitGroup.get(i).isTouching(players)){
+                            fruitGroup.get(i).destroy();
                             player.score+=1;
                             player.update();
                             
